77abec7e58483e6a35cd849bcd189c1abb3b4296
masato <04masato05@gmail.com>
2024-11-03 22:29:18 +0900

    GAタグを読み込むように

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,17 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/tailwindcss',
-    '@zadigetvoltaire/nuxt-gtm'
+    '@nuxtjs/tailwindcss'
   ],
-  runtimeConfig: {
-    public: {
-      gtmId: process.env.GOOGLE_TAG_MANAGER_ID || 'GTM-XXXXXXX'
-    }
-  },
-
-  gtm: {
-    id: process.env.GOOGLE_TAG_MANAGER_ID || 'GTM-XXXXXXX',
-    enabled: !!process.env.GOOGLE_TAG_MANAGER_ID
-  }
 })
---
+++ plugins/vue-tag.client.ts
@@ -0,0 +1,13 @@
+import VueGtag, { trackRouter } from "vue-gtag-next";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const GA_ID = process.env.GA_ID;
+  if (GA_ID) {
+    nuxtApp.vueApp.use(VueGtag, {
+        property: {
+          id: "GA_MEASUREMENT_ID",
+        },
+      });
+      trackRouter(useRouter());
+  }
+});
