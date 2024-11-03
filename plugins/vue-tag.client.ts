65fe110b6aaadfb2e6f6eaa5d02210d47324e653
masato <04masato05@gmail.com>
2024-11-03 23:03:19 +0900

    デバッグコマンド削除漏れ対応

--- plugins/vue-tag.client.ts
+++ plugins/vue-tag.client.ts
@@ -3,7 +3,6 @@ import VueGtag, { trackRouter } from "vue-gtag-next";
 export default defineNuxtPlugin((nuxtApp) => {
 const config = useRuntimeConfig()
 const GA_ID = config.public.GA_ID
-console.log(GA_ID);
   if (GA_ID) {
     nuxtApp.vueApp.use(VueGtag, {
         property: {
