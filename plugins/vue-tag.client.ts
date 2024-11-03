cfd4cfd09bfc22835234984031765042b32a6b75
masato <04masato05@gmail.com>
2024-11-03 22:52:44 +0900

    デバッグコマンド追加

--- plugins/vue-tag.client.ts
+++ plugins/vue-tag.client.ts
@@ -3,7 +3,9 @@ import VueGtag, { trackRouter } from "vue-gtag-next";
 export default defineNuxtPlugin((nuxtApp) => {
 const config = useRuntimeConfig()
 const GA_ID = config.public.GA_ID
+console.log(GA_ID);
   if (GA_ID) {
+    console.log("取得できたよ");
     nuxtApp.vueApp.use(VueGtag, {
         property: {
           id: GA_ID,
