9af304ad1d08130a506774f8373bfe1f1c98df62
masato <04masato05@gmail.com>
2024-11-03 23:02:57 +0900

    デバッグコマンド削除

--- plugins/vue-tag.client.ts
+++ plugins/vue-tag.client.ts
@@ -5,7 +5,6 @@ const config = useRuntimeConfig()
 const GA_ID = config.public.GA_ID
 console.log(GA_ID);
   if (GA_ID) {
-    console.log("取得できたよ");
     nuxtApp.vueApp.use(VueGtag, {
         property: {
           id: GA_ID,
