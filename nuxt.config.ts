07603a2d7ce14141e0390d8bf694acd3a145495a
masato <04masato05@gmail.com>
2024-11-04 09:49:33 +0900

    サイトマップモジュールを追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/sitemap'],
   runtimeConfig: {
     public: {
       GA_ID: process.env.GA_ID || ''
     }
+  },
+  site: {
+    url: 'https://dev-tools.click/',
+    name: 'Dev Tools'
   }
 })
